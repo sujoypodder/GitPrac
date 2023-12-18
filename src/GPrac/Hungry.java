76d273d479a126c42234f97bdcfc63bbbae6b26f
Sujoy <Sujoy@DESKTOP-BCCV5NO>
2023-12-19 01:08:04 +0600

    First Commit Practice Mode

---
+++ src/GPrac/Hungry.java
@@ -0,0 +1,12 @@
+package GPrac;
+
+public class Hungry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20;
+		int c=a+b;
+		System.out.print(c);
+	}
+
+}
